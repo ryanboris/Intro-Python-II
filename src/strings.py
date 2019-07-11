43e874ab61b3d4882725a6745292fae7339120a6
ryanboris <ryan.boris@gmail.com>
2019-07-11 16:49:31 -0400

    :bento: Add more strings for mystery games

--- src/strings.py
+++ src/strings.py
@@ -1,4 +1,3 @@
 
 strings = ['hello world how are you?',
-           'Null is nothing to worry about.', 'Did you pull the repo?']
-
+           'Null is nothing to worry about.', 'Did you pull the repo?', 'Toast is good for breakfast.', 'Netlify gives me cramps.', 'Heroku to you too!', 'Well, life is just grande aint it?', 'And you are?']
