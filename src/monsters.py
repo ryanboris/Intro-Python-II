b8887b5de6898363bfeba81989c956cb1e6c62cb
ryanboris <ryan.boris@gmail.com>
2019-07-11 14:55:01 -0400

    :construction: Add more rooms for stretch

--- src/monsters.py
+++ src/monsters.py
@@ -1,5 +1,6 @@
 class Monster:
-    def __init__(self, name, description, current_room):
+    def __init__(self, name, description, current_room, hp):
         self.name = name
         self.description = description
         self.current_room = current_room
+        self.hp = hp
